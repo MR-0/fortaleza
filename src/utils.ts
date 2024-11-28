751459ab491b4cee725356ae6b7a22cdefa86e1a
Mario Rojas <mario.rojas@bain.com>
2024-11-27 23:11:44 -0300

    feat: improve to json function

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,10 @@
 export function toJson(str: string) {
   console.log('json inital ->', str)
+  const hasJsonBlock = /^\s*```json\s*/.test(str)
   str = str.replace(/^\s*```json\s*/, '')
-  str = str.replace(/\s*```\s*$/, '')
+  str = hasJsonBlock
+    ? str.split(/\s*```\s*/).shift() || '{}'
+    : str
   console.log('json final ->', str)
   try {
     return JSON.parse(str)
