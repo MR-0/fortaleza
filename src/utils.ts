ddd42620c31a74abdb54458d4bc5aaa6caaa43a6
Mario Rojas <mario.rojas@bain.com>
2024-11-27 22:44:07 -0300

    fix: to json function end reg exp

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export function toJson(str: string) {
   console.log('json inital ->', str)
   str = str.replace(/^\s*```json\s*/, '')
-  str = str.replace(/$\s*```\s*/, '')
+  str = str.replace(/\s*```\s*$/, '')
   console.log('json final ->', str)
   try {
     return JSON.parse(str)
