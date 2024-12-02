21f201ea8cd17b2b9e4f35012af85f691a58fb4c
Mario Rojas <mario.rojas@bain.com>
2024-12-02 18:16:49 -0300

    chore: remove to json function logs

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,9 @@
 export function toJson(str: string) {
-  console.log('json inital ->', str)
   const hasJsonBlock = /^\s*```json\s*/.test(str)
   str = str.replace(/^\s*```json\s*/, '')
   str = hasJsonBlock
     ? str.split(/\s*```\s*/).shift() || '{}'
     : str
-  console.log('json final ->', str)
   try {
     return JSON.parse(str)
   }
