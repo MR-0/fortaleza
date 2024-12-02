fedefde3900737d3bebe16eeae914160964111ca
Mario Rojas <mario.rojas@bain.com>
2024-12-02 20:00:37 -0300

    feat: add chat input styles

--- src/chatInput.ts
+++ src/chatInput.ts
@@ -1,5 +1,6 @@
 import { VirtualNode } from "./vnode";
 import style from './chatInput.module.css'
+import flourishSvg from '../assets/flourish.svg?raw'
 
 export class ChatInput extends VirtualNode {
   private _input
@@ -9,10 +10,25 @@ export class ChatInput extends VirtualNode {
     this.class(style.form)
 
     this.add('p')
+      .class(style.question)
       .content('What do you want to do?')
 
-    this._input = this.add<HTMLInputElement>('input')
+    this.add('div')
+      .class(style.flourish)
+      .class(style.flourishTop)
+      .content(flourishSvg)
+
+    this._input = this.add('div')
+      .class(style.inputWrapper)
+      .add<HTMLTextAreaElement>('textarea')
+      .class(style.input)
       .attrs({ type: 'text' })
+
+    this.add('div')
+      .class(style.flourish)
+      .class(style.flourishBottom)
+      .content(flourishSvg)
+
   }
 
   submit(handler: (prompt: string) => Promise<void>) {
@@ -21,6 +37,16 @@ export class ChatInput extends VirtualNode {
       const { value: prompt } = this._input.dom
       this._input.dom.value = ''
       if (prompt) handler(prompt)
+    })
+
+    this._input.on('keydown', (e) => {
+      const ke = e as KeyboardEvent
+      if (ke.key === 'Enter' && !ke.shiftKey) {
+        e.preventDefault()
+        const { value: prompt } = this._input.dom
+        this._input.dom.value = ''
+        if (prompt) handler(prompt)
+      }
     })
 
   }
